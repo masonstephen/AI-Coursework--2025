d02eb9927a60dbc494aa88139795b9f541f2b80d
113832 <stephen.mason@strathmore.edu>
2025-08-28 12:04:33 +0300

    Remove all files from repository

--- Question_1.py
+++
@@ -1,8 +0,0 @@
-# The line below will ask the user for the number of days
-days = int(input("Enter the number of days: "))
-
-# Calculate the number of seconds in the given number of days inputed by the user
-seconds = days * 24 * 60 * 60
-
-# Print the result
-print(f"The number of seconds in {days} days is {seconds} seconds.")
--- Question_2.py
+++
@@ -1,8 +0,0 @@
-# Input the radius
-radius = float(input("Enter the radius of the sphere: "))
-
-# Calculate the volume of the sphere using the formula (4/3)πr^3
-volume = (4/3) * 3.14 * (radius ** 3)
-
-# Print the result
-print(f"The volume of the sphere with radius {radius} is {volume:.2f} cubic units.")
